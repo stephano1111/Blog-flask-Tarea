d05d25082326cfba238f46a61f7f54819b144f35
stephano1111 <stephanods@hotmail.com>
2022-04-06 18:44:05 -0500

    Register/Sesion html

--- app.py
+++ app.py
@@ -9,3 +9,11 @@ def home():
 @app.route('/enter')
 def enter_blog():
     return render_template('enter.html')
+
+@app.route('/register')
+def register_blog():
+    return render_template('register.html')
+
+@app.route('/sesion')
+def sesion_blog():
+    return render_template('sesion.html')
