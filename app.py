be464ffa323e0111a68322ad2e2881d466593055
stephano1111 <stephanods@hotmail.com>
2022-04-06 20:21:40 -0500

    BD agregado

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template#Servidor
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
-app = Flask(__name__)
+app = Flask(__name__)#Servidor
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+db = SQLAlchemy(app)
+mm = Marshmallow(app)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    content = db.Column(db.String(1000))
+    created = db.Column(db.DateTime())
+
+    def __init__(self, title, content, created):
+        self.title = title
+        self.content = content
+        self.created = created
+    
+    def __repr__(self):
+        return f'<Post {self.title}>'
+
+#Servidor
 @app.route('/')
 def home():
     return render_template('home/index.html')
